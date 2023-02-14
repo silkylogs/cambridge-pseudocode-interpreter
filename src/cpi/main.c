5f9e5187c627989d4c5c0766011f876182af185e
silkylogs <87656538+silkylogs@users.noreply.github.com>
2023-02-14 19:44:55 +0600

    Tested adding a new user defined library

--- src/cpi/main.c
+++ src/cpi/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "cpistr.h"
+#include "cpi_iee.h"
 
 int main(){
 	CpiString *helloWorldStr = NULL;
 	printf("initializing test string...\n");
 	initNewStringObj(&helloWorldStr, "Hello world");
 	printf("\"%s\"\n", getString(helloWorldStr));
+
+	CPI_IEE_foo();
 	return 0;
 }
---
+++ src/cpi_iee/cpi_iee.c
@@ -0,0 +1,4 @@
+#include "cpi_iee.h"
+
+
+void CPI_IEE_foo(){ int a = 5+5; puts("23"); }
