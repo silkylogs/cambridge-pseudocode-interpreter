640f89358136cc928cdf6755e0485a04bdd6cc53
silkylogs <silkylogs@gmail.com>
2023-12-23 15:39:26 +0530

    main: setup primitive testing infrastructure

--- main.c
+++ main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef signed long long int ssize;
 
+#define CP_LOG(WARN_LEVEL, MSG) \
+  printf("%s:%d:0: %s: %s\n", __FILE__, __LINE__, WARN_LEVEL, MSG)
+
 #include "CpAsciiStringView.c"
 
-/*
-struct CpToken {
-  enum CpTokenType {
-    CP_TOKEN_TYPE_ATOMIC_TYPE,
-  } token_type;
-};
-*/
+// get atomic type name
 
-int main(void){
-  struct CpAsciiStringView str = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR("Hello, world!\n");
-  printf("%s", str.chars);
+//
+
+bool cp_run_all_tests(void);
+bool cp_run_all_tests(void) {
+  bool all_ok = true, current_ok = true;
+  
+  current_ok &= true; // insert function here
+  all_ok &= current_ok;
+    
+  return all_ok;
+}
+
+int main(void) {
+  if (!cp_run_all_tests()) {
+    CP_LOG("Error", "One or more tests returned failure");
+    return -1;
+  }
   return 0;
 }
