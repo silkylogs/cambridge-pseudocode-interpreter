4f30423aaff2e0690a06d2f42c62e13034ac00c6
silkylogs <silkylogs@gmail.com>
2023-12-31 12:30:19 +0600

    Move logging and assert functions to own file

--- main.c
+++ main.c
@@ -3,30 +3,7 @@
 
 typedef signed long long int ssize;
 
-#define CP_LOG(WARN_LEVEL, MSG)						\
-    printf("%s:%d:1: %s: %s\n", __FILE__, __LINE__, WARN_LEVEL, MSG)
-
-#define CP_REPORT(PRINTF_FMT_STR, ...)			\
-    printf("%s:%d:1: Warning: " PRINTF_FMT_STR "\n",	\
-	   __FILE__, __LINE__, __VA_ARGS__);
-
-bool cp_assert_internal(bool b, const char *b_expression, const char *file, int line);
-bool cp_assert_internal(bool b, const char *b_expression, const char *file, int line) {
-    if (!b)
-	printf("%s:%d:1: Assertion failure:\n"
-	       "---------- Expression starts here: ----------\n"
-	       "%s\n"
-	       "---------- Expression ends here.   ----------\n",
-	       file, line, b_expression);
-    return b;
-}
-
-#define CP_ASSERT(EXPR) cp_assert_internal((EXPR), #EXPR, __FILE__, __LINE__)
-
-#define CP_ASSERT_SET(EXPR, BOOL)		\
-    CP_ASSERT(EXPR);				\
-    BOOL = (EXPR)
-
+#include "CpInternalLoggingUtils.c"
 #include "CpAsciiStringView.c"
 
 
