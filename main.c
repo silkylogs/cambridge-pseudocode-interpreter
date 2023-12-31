14310a14c4d655f2513e927be6b133911ce9309a
silkylogs <silkylogs@gmail.com>
2023-12-31 14:16:33 +0600

    Add CpVariableManager

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef signed long long int ssize;
 
 #include "CpInternalLoggingUtils.c"
 #include "CpAsciiStringView.c"
-
+#include "CpVariableManager.c"
 
 bool cp_test_internal_atomic_type_sizes(void);
 bool cp_test_internal_atomic_type_sizes(void) {
     return sizeof (ssize) == 8;
 }
+
+/*
+// Memory
+    struct CpUniquePtr {
+	void *data;
+	ssize data_len_bytes;
+	bool is_valid;
+    } data;
+
+#define CPUNIQUEPTR_NULLPTR (struct CpUniquePtr){ .data=NULL; .data_len_bytes=0; .is_valid=false; }
+// Memory
+*/
+
+
 
 bool cp_run_all_tests(void);
 bool cp_run_all_tests(void) {
@@ -19,6 +34,7 @@ bool cp_run_all_tests(void) {
     all_ok &= CP_ASSERT(cp_test_internal_atomic_type_sizes());
     all_ok &= CP_ASSERT(cp_test_CpAsciiStringView_from_const_cstr());
     all_ok &= CP_ASSERT(cp_test_CpAsciiStringView_match_case_sensitive_substring_in_string_unchecked());
+    all_ok &= CP_ASSERT(cp_test_CpVariableManager_push_var_to_stack_unchecked());
     
     return all_ok;
 }
