d98dc601fe421a1181bfddea0469dbaaf207daf7
silkylogs <87656538+silkylogs@users.noreply.github.com>
2025-01-05 19:11:02 +0600

    Remove unnessecary files

--- main.c
+++ main.c
@@ -111,6 +111,439 @@ bool test_succeed(void) {
     return 1 + 1 == 2;
 }
 
+// -- Variable manager ---
+//
+// A vestigial module intended to manage variable data scheduled for removal.
+
+struct CpVariableInfo {
+	struct CpAsciiStringView name;
+	struct CpVariableType type;
+	// TODO: associate data?
+};
+
+#define CPVARIABLEMANAGER_VAR_STACK_SIZE ((ssize)(16))
+struct CpVariableManager {
+	ssize stack_idx;
+	struct CpVariableInfo var_stack[CPVARIABLEMANAGER_VAR_STACK_SIZE];
+};
+
+void CpVariableManager_push_var_to_stack_unchecked(
+	struct CpVariableManager *manager,
+	struct CpAsciiStringView new_name,
+	struct CpVariableType new_type);
+
+void CpVariableManager_push_var_to_stack_unchecked(
+	struct CpVariableManager *manager,
+	struct CpAsciiStringView new_name,
+	struct CpVariableType new_type)
+{
+	manager->var_stack[manager->stack_idx].name = new_name;
+	manager->var_stack[manager->stack_idx].type = new_type;
+	manager->stack_idx++;
+}
+
+bool CpVariableManager_push_var_to_stack_check(struct CpVariableManager *manager);
+bool CpVariableManager_push_var_to_stack_check(struct CpVariableManager *manager)
+{
+	if (manager->stack_idx >= CPVARIABLEMANAGER_VAR_STACK_SIZE)
+	return false;
+
+	return true;
+}
+
+bool cp_test_CpVariableManager_push_var_to_stack_unchecked(void);
+bool cp_test_CpVariableManager_push_var_to_stack_unchecked(void) {
+	struct CpVariableManager manager = {0};
+	bool all_ok = true;
+
+	// Test general pushes
+	struct CpAsciiStringView dummy_name = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED("DummyVar");
+	struct CpVariableType dummy_type = {
+	.name = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED("DummyType"),
+	.id = 0,
+	};
+	CpVariableManager_push_var_to_stack_unchecked(&manager, dummy_name, dummy_type);
+
+	ssize last_idx = manager.stack_idx - 1;
+	all_ok &= CP_ASSERT(
+	CpAsciiStringView_equals(
+		manager.var_stack[last_idx].name,
+		dummy_name));
+	
+	all_ok &= CP_ASSERT(
+	0 == memcmp(
+		&manager.var_stack[last_idx].type,
+		&dummy_type,
+		sizeof (struct CpVariableType)));
+	
+	
+	return all_ok;
+}
+
+/*
+bool cp_test_variable_management(void) {    
+	char *declaration_tests[] = {
+	"DECLARE Counter : INTEGER",
+	"DECLARE TotalToPay : REAL",
+	"DECLARE InputChar : CHAR",
+	"DECLARE PlayerName: STRING",
+	"DECLARE GameOver : BOOLEAN",
+	"DECLARE SaveDate : DATE",
+	};
+	ssize len = sizeof declaration_tests / sizeof declaration_tests[0];
+	
+	for (ssize i = 0; i < len; ++i)
+	printf("%d/%d: \"%s\"\n", i+1, len, declaration_tests[i]);
+	return true;
+}
+*/
+
+// -- Type manager ----
+//
+// A vestigial module intended to manage type data scheduled for removal.
+
+#ifndef CP_SOURCE_TYPE_MANAGER
+#define CP_SOURCE_TYPE_MANAGER
+
+/*
+// Memory
+	struct CpUniquePtr {
+	void *data;
+	ssize data_len_bytes;
+	bool is_valid;
+	} data;
+
+#define CPUNIQUEPTR_NULLPTR (struct CpUniquePtr){ .data=NULL; .data_len_bytes=0; .is_valid=false; }
+// Memory
+*/
+
+	/* Indices double as the type ID */
+struct CpTypeStack {
+	char *backing_name_array;
+	ssize backing_name_array_len;
+	
+	ssize *backing_name_size_array;
+	ssize backing_name_size_array_len;
+	
+	ssize names_last_idx;
+};
+
+void CpTypeStack_init_with_size_unchecked(
+	struct CpTypeStack *tstack,
+	char *new_backing_name_array,
+	ssize new_backing_name_array_len,
+	ssize *new_backing_name_size_array,
+	ssize new_backing_name_size_array_len);
+void CpTypeStack_init_with_size_unchecked(
+	struct CpTypeStack *tstack,
+	char *new_backing_name_array,
+	ssize new_backing_name_array_len,
+	ssize *new_backing_name_size_array,
+	ssize new_backing_name_size_array_len)
+{
+	tstack->backing_name_array_len = new_backing_name_array_len;
+	tstack->backing_name_size_array_len = new_backing_name_size_array_len;
+	
+	tstack->backing_name_array = new_backing_name_array;
+	tstack->backing_name_size_array = new_backing_name_size_array;
+	
+	tstack->names_last_idx = 0;
+}
+
+
+bool CpTypeStack_init_with_size_check(
+	struct CpTypeStack *tstack,
+	char *new_backing_name_array,
+	ssize new_backing_name_array_len,
+	ssize *new_backing_namelen_array,
+	ssize  new_backing_namelen_array_len);
+
+bool CpTypeStack_init_with_size_check(
+	struct CpTypeStack *tstack,
+	char *new_backing_name_array,
+	ssize new_backing_name_array_len,
+	ssize *new_backing_namelen_array,
+	ssize  new_backing_namelen_array_len)
+{
+	bool is_tstack_ptr_nonnull, is_struct_zeroed,
+	isvalid_new_backing_name_array,
+	isvalid_new_backing_name_array_len,
+	isvalid_new_backing_namelen_array,
+	isvalid_new_backing_namelen_array_len;
+
+	struct CpTypeStack zeroed_tstack;
+	memset(&zeroed_tstack, 0, sizeof (struct CpTypeStack));
+
+	is_tstack_ptr_nonnull = tstack != NULL;
+	if (!is_tstack_ptr_nonnull) return false;
+
+	is_struct_zeroed =
+	0 == memcmp(&zeroed_tstack, tstack, sizeof (struct CpTypeStack));
+
+	isvalid_new_backing_name_array = NULL != new_backing_name_array;
+	isvalid_new_backing_name_array_len = new_backing_name_array_len > 0;
+	
+	isvalid_new_backing_namelen_array = NULL != new_backing_namelen_array;
+	isvalid_new_backing_namelen_array_len = new_backing_namelen_array_len > 0;
+	
+	return is_tstack_ptr_nonnull
+	&& is_struct_zeroed
+	&& isvalid_new_backing_name_array
+	&& isvalid_new_backing_name_array_len
+	&& isvalid_new_backing_namelen_array
+	&& isvalid_new_backing_namelen_array_len;
+}
+
+bool cp_test_CpTypeStack_init_with_size(void);
+bool cp_test_CpTypeStack_init_with_size(void) {
+	bool all_ok = true;
+
+	/* Invalid sizes */ {
+	struct CpTypeStack valid_stack;
+	char valid_backing_name_array[0xff];
+	ssize valid_backing_namelen_array[0xff];
+	
+	ssize invalid_size_negative = -23544325;
+	ssize valid_size = 0xff;
+
+	memset(&valid_stack, 0, sizeof (struct CpTypeStack));
+
+	all_ok &= CP_ASSERT(
+		!CpTypeStack_init_with_size_check(
+		&valid_stack,
+		valid_backing_name_array, invalid_size_negative,
+		valid_backing_namelen_array, invalid_size_negative));
+
+	}
+	
+	/* Non zeroed struct */ {
+	struct CpTypeStack invalid_stack;
+	char valid_backing_name_array[0xff];
+	ssize valid_backing_namelen_array[0xff];
+	ssize valid_size = 0xff;
+
+	all_ok &= CP_ASSERT(
+		!CpTypeStack_init_with_size_check(
+		&invalid_stack,
+		valid_backing_name_array, valid_size,
+		valid_backing_namelen_array, valid_size));
+	}
+	
+	/* Invalid pointers */ {
+	struct CpTypeStack invalid_stack;
+	char invalid_backing_name_array[0xff];
+	ssize valid_backing_namelen_array[0xff];
+	ssize valid_size = 0xff;
+
+	all_ok &= CP_ASSERT(
+		!CpTypeStack_init_with_size_check(
+		&invalid_stack,
+		invalid_backing_name_array, valid_size,
+		valid_backing_namelen_array, valid_size));	
+	}
+	
+	return all_ok;
+}
+
+bool cp_test_CpTypeManager(void);
+bool cp_test_CpTypeManager(void) {
+	bool all_ok = true;
+	
+	all_ok &= CP_ASSERT(cp_test_CpTypeStack_init_with_size());
+	
+	return all_ok;
+}
+
+#endif
+
+// -- String view ----
+//
+// A vestigial module scheduled for removal.
+
+#ifndef CP_SRC_ASCII_STRING_VIEW
+#define CP_SRC_ASCII_STRING_VIEW
+
+struct CpAsciiStringView {
+	char *chars;
+	ssize len_including_nul;
+};
+
+// -- from_const_cstr --------------------------
+
+struct CpAsciiStringView CpAsciiStringView_from_const_cstr_unchecked(const char *cstr, ssize cstr_len);
+bool CpAsciiStringView_from_const_cstr_check(const char *cstr, ssize cstr_len);
+bool CpAsciiStringView_check_well_formed(struct CpAsciiStringView str);
+bool cp_test_CpAsciiStringView_from_const_cstr(void);
+
+
+struct CpAsciiStringView
+CpAsciiStringView_from_const_cstr_unchecked(const char *cstr, ssize cstr_len) {
+	return (struct CpAsciiStringView) {
+		.chars = (char *)cstr,
+		.len_including_nul = cstr_len,
+	};
+}
+
+#define CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED(CSTR)		\
+	CpAsciiStringView_from_const_cstr_unchecked((CSTR), sizeof (CSTR))
+
+
+bool
+CpAsciiStringView_from_const_cstr_check(const char *cstr, ssize cstr_len) {
+	if (cstr == NULL) return false;
+	if (cstr_len < 0) return false;
+	return true;
+}
+
+#define CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_CHECK(CSTR)			\
+	CpAsciiStringView_from_const_cstr_check((CSTR), sizeof (CSTR))
+
+
+// Note: This exists in the first place because I thought some casting may be required
+//       in order to call const_cstr_check
+bool CpAsciiStringView_check_well_formed(struct CpAsciiStringView str) {
+	return CpAsciiStringView_from_const_cstr_check(str.chars, str.len_including_nul);
+}
+
+
+bool cp_test_CpAsciiStringView_from_const_cstr(void) {
+	// Test the macro checks for obvious defects  
+	CP_ASSERT(false == CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_CHECK(NULL));
+	CP_ASSERT(true == CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_CHECK(""));
+
+	struct CpAsciiStringView test;
+	test = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED("This string has 25 chars");
+	CP_ASSERT(test.len_including_nul == 25);
+	CP_ASSERT(test.chars != NULL);
+  
+	return true;
+}
+
+// -- from_const_cstr --------------------------
+
+
+// -- substring --------------------------------
+
+struct CpAsciiStringView CpAsciiStringView_substring_unchecked(
+	struct CpAsciiStringView string,
+	ssize idx1, ssize idx2);
+bool CpAsciiStringView_substring_check(
+	struct CpAsciiStringView string,
+	ssize idx1, ssize idx2);
+
+
+struct CpAsciiStringView
+CpAsciiStringView_substring_unchecked(struct CpAsciiStringView string, ssize idx1, ssize idx2) {
+	if (idx1 > idx2) { ssize temp = idx1; idx1 = idx2; idx2 = temp; }
+
+	struct CpAsciiStringView substr = {
+	.chars = &string.chars[idx1],
+	.len_including_nul = idx2 - idx1
+	};
+
+	return substr;
+}
+
+
+
+bool
+CpAsciiStringView_substring_check(struct CpAsciiStringView string, ssize idx1, ssize idx2) {
+	// Out of bounds check
+	if (idx1 < 0 || idx1 >= string.len_including_nul) return false;
+	if (idx2 < 0 || idx2 >= string.len_including_nul) return false;
+
+	// Source string anomaly check
+	if (!CpAsciiStringView_check_well_formed(string)) return false;
+
+	return true;
+}
+
+// -- substring --------------------------------
+
+
+struct CpAsciiStringView_StringSubstringPair {
+	struct CpAsciiStringView string;
+	struct CpAsciiStringView substring;
+};
+
+bool CpAsciiStringView_match_case_sensitive_substring_in_string_unchecked(
+	struct CpAsciiStringView_StringSubstringPair);
+
+bool
+CpAsciiStringView_match_case_sensitive_substring_in_string_unchecked(
+	struct CpAsciiStringView_StringSubstringPair pair)
+{
+	struct CpAsciiStringView string = pair.string;
+	struct CpAsciiStringView substring = pair.substring;
+	
+	if (substring.len_including_nul > string.len_including_nul) return false;
+	for (ssize str_i = 0; str_i < string.len_including_nul; ++str_i) {
+	if (string.chars[str_i] == substring.chars[0]) {
+		for (ssize sub_i = 0;
+		 sub_i < substring.len_including_nul && sub_i < substring.len_including_nul;
+		 ++sub_i)
+		{
+		char str_char = string.chars[str_i + sub_i];
+		char sub_char = substring.chars[sub_i];
+		
+		//printf("Comparing \'%c\' with \'%c\'\n", str_char, sub_char);
+		if (sub_char == '\0') return true;
+		if (str_char != sub_char) return false;
+		}
+	}
+	}
+	return true;
+}
+
+#define CP_ASCIISTRINGVIEW_MATCH_CASE_SENSITIVE_SUBSTR_IN_STR_UNCHECKED(...)	\
+	CpAsciiStringView_match_case_sensitive_substring_in_string_unchecked( \
+	(struct CpAsciiStringView_StringSubstringPair) {		\
+		__VA_ARGS__							\
+	})
+
+// Tests
+bool
+cp_test_CpAsciiStringView_match_case_sensitive_substring_in_string_unchecked(void);
+bool
+cp_test_CpAsciiStringView_match_case_sensitive_substring_in_string_unchecked(void) {
+	bool all_ok = true;
+
+	// Tests for intended usage
+	all_ok &= CP_ASSERT(
+		CP_ASCIISTRINGVIEW_MATCH_CASE_SENSITIVE_SUBSTR_IN_STR_UNCHECKED(
+		.string = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED("DECLARE TestVar: BOOL"),
+		.substring = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED("BOOL"))
+	);
+
+	all_ok &= CP_ASSERT(
+		CP_ASCIISTRINGVIEW_MATCH_CASE_SENSITIVE_SUBSTR_IN_STR_UNCHECKED(
+		.substring = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED("INTEGER"),
+		.string = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED("INTEGER64"))
+	);
+
+	// Improper usage: substring longer than original string
+	all_ok &= CP_ASSERT(
+		!CP_ASCIISTRINGVIEW_MATCH_CASE_SENSITIVE_SUBSTR_IN_STR_UNCHECKED(
+		.string = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED(
+		"Obviously long test string"),
+		.substring = CP_ASCIISTRINGVIEW_FROM_CONST_CSTR_UNCHECKED(
+		"Obviously longer test string"))
+	);
+
+	return all_ok;
+}
+
+bool CpAsciiStringView_equals(struct CpAsciiStringView a, struct CpAsciiStringView b);
+bool CpAsciiStringView_equals(struct CpAsciiStringView a, struct CpAsciiStringView b) {
+	if (a.len_including_nul != b.len_including_nul) return false;
+	ssize len = a.len_including_nul;
+
+	return 0 == memcmp(a.chars, b.chars, len);
+}
+
+#endif
+
+
 // -- Lexer ----
 //
 // Converts unformatted string input into a stream of tokens.
