213d458802259ca89e46792974691dbb29a557bb
silkylogs <silkylogs@gmail.com>
2025-02-07 16:58:42 +0600

    Add more warnings, Enable fatal warnings

--- premake5.lua
+++ premake5.lua
@@ -24,6 +24,7 @@ project "CambridgePseudocodeInterpreter"
    filter "system:windows"
       warnings "Everything"
       cdialect "C99"
+      flags { "FatalCompileWarnings" }
       fatalwarnings {
          4020, -- Function has too many arguments
          4024, -- Different types for formal and actual parameter
@@ -33,12 +34,17 @@ project "CambridgePseudocodeInterpreter"
          4131, -- Function uses old-style declarator
          4477, -- Mismatched format string and variadic argument type
          4052, -- function declarations different; one contains variable arguments
+         4715, -- not all control paths return a value
+         4716, -- Function must return a value
+         4013, -- Function undefined; assuming extern returning int
       }
       disablewarnings {
          4777, -- 'printf' : format string
          4820, -- padding added after data member
          5045, -- Compiler will insert Spectre mitigation for memory load
          4100, -- unreferenced formal parameter
+         4189, -- local variable is initialized but not referenced
+         4101, -- unreferenced local variable
       }
 
    filter "system:linux"
