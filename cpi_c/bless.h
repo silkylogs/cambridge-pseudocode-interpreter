2cdf996c5d46026007704dd00f0b99a3500f0bb4
silkylogs <silkylogs@gmail.com>
2026-01-02 13:01:21 +0600

    Replace double with char*
    Plan to represent numeric types with null terminated strings.

--- cpi_c/bless.h
+++ cpi_c/bless.h
@@ -69,95 +69,95 @@
 // - Record: get, put
 
 struct Instr {
-	enum Kind {
-		DECL_VAR, DECL_CONST, DECL_ARR1D, DECL_ARR2D,
+    enum Kind {
+        DECL_VAR, DECL_CONST, DECL_ARR1D, DECL_ARR2D,
 
-		ASS_ADD, ASS_SUB, ASS_MUL, ASS_DIV, ASS_GEQ, ASS_LEQ, ASS_GT, ASS_LT, ASS_EQ, ASS_NEQ, ASS_AND, ASS_OR, ASS_NOT,
+        ASS_ADD, ASS_SUB, ASS_MUL, ASS_DIV, ASS_GEQ, ASS_LEQ, ASS_GT, ASS_LT, ASS_EQ, ASS_NEQ, ASS_AND, ASS_OR, ASS_NOT,
 
-		BR_IF, BR_IF_ELSE, BR_CASE, BR_CASE_OTHERWISE,
+        BR_IF, BR_IF_ELSE, BR_CASE, BR_CASE_OTHERWISE,
 
-		LOOP_FOR, LOOP_FOR_STEP, LOOP_REPEAT_UNTIL, LOOP_WHILE,
+        LOOP_FOR, LOOP_FOR_STEP, LOOP_REPEAT_UNTIL, LOOP_WHILE,
 
-		DEFN_CUSTOM_TYPE, DEFN_PROCEDURE, DEFN_FUNCTION,
+        DEFN_CUSTOM_TYPE, DEFN_PROCEDURE, DEFN_FUNCTION,
 
-		CMD_CALL, CMD_INPUT, CMD_OUTPUT, CMD_FILE_OPEN, CMD_FILE_READ, CMD_FILE_WRITE, CMD_FILE_CLOSE, CMD_FILE_SEEK, CMD_RECORD_GET, CMD_RECORD_PUT,
-	} kind;
+        CMD_CALL, CMD_INPUT, CMD_OUTPUT, CMD_FILE_OPEN, CMD_FILE_READ, CMD_FILE_WRITE, CMD_FILE_CLOSE, CMD_FILE_SEEK, CMD_RECORD_GET, CMD_RECORD_PUT,
+    } kind;
 
-	// Parameters. Use whatever parameter from here where appropriate.
-	// Note: Strings are null terminated.
-	char *param_decl_var_name;
-	char *param_decl_var_type;
+    // Parameters. Use whatever parameter from here where appropriate.
+    // Note: Strings are null terminated.
+    char *param_decl_var_name;
+    char *param_decl_var_type;
 
-	char *param_decl_const_name;
-	char *param_decl_const_type;
+    char *param_decl_const_name;
+    char *param_decl_const_type;
 
-	char *param_decl_arr1d_name;
-	char *param_decl_arr1d_type;
-	double param_decl_arr1d_bound_lower;
-	double param_decl_arr1d_bound_upper;
+    char *param_decl_arr1d_name;
+    char *param_decl_arr1d_type;
+    char *param_decl_arr1d_bound_lower;
+    char *param_decl_arr1d_bound_upper;
 
-	char *param_decl_arr2d_name;
-	char *param_decl_arr2d_type;
-	double param_decl_arr2d_dim0_bound_lower;
-	double param_decl_arr2d_dim0_bound_upper;
-	double param_decl_arr2d_dim1_bound_lower;
-	double param_decl_arr2d_dim1_bound_upper;
+    char *param_decl_arr2d_name;
+    char *param_decl_arr2d_type;
+    char *param_decl_arr2d_dim0_bound_lower;
+    char *param_decl_arr2d_dim0_bound_upper;
+    char *param_decl_arr2d_dim1_bound_lower;
+    char *param_decl_arr2d_dim1_bound_upper;
 
-	// The operation type (add, sub, etc) should be derived from the kind of operation.
-	char *param_ass_result_name;
-	char *param_ass_param0_name;
-	char *param_ass_param1_name;
+    // The operation type (add, sub, etc) should be derived from the kind of operation.
+    char *param_ass_result_name;
+    char *param_ass_param0_name;
+    char *param_ass_param1_name;
 
-	// "If condition is true, execute this *reigon* of code. Else skip over that reigon.
-	char *param_br_if_condition;
-	double param_br_if_reigon_len;
-	double param_br_if_else_reigon_len; // if (foo) ... else ... [The else conditoin cannot exist without the if]
-	
-	double param_br_case_condition;
-	double param_br_case_reigon_len;
-	char **param_br_case_values;
-	double *param_br_case_value_reigons;
-	double param_br_case_value_cnt;
+    // "If condition is true, execute this *reigon* of code. Else skip over that reigon.
+    char *param_br_if_condition;
+    char *param_br_if_reigon_len;
+    char *param_br_if_else_reigon_len; // if (foo) ... else ... [The else conditoin cannot exist without the if]
 
-	char *loop_for_identifier_name;
-	char *loop_for_value0_name;
-	char *loop_for_value1_name;
-	char *loop_for_step_name;
-	double loop_for_reigon_len;
+    char *param_br_case_condition;
+    char *param_br_case_reigon_len;
+    char **param_br_case_values;
+    char **param_br_case_value_reigons;
+    char *param_br_case_value_cnt;
 
-	char *loop_repeat_until_condition_name;
-	double loop_repeat_until_reigon_len;
+    char *loop_for_identifier_name;
+    char *loop_for_value0_name;
+    char *loop_for_value1_name;
+    char *loop_for_step_name;
+    char *loop_for_reigon_len;
 
-	char *loop_while_condition_name;
-	double loop_while_reigon_len;
+    char *loop_repeat_until_condition_name;
+    char *loop_repeat_until_reigon_len;
 
-	char *defn_custom_type_name;
-	char *defn_custom_type_type;
+    char *loop_while_condition_name;
+    char *loop_while_reigon_len;
 
-	char *defn_procedure_name;
-	double defn_procedure_reigon_len;
+    char *defn_custom_type_name;
+    char *defn_custom_type_type;
 
-	char *defn_function_name;
-	char *defn_function_ret_value_name;
-	double defn_function_reigon_len;
+    char *defn_procedure_name;
+    char *defn_procedure_reigon_len;
 
-	char *cmd_call_name;
-	char *cmd_input_name;
-	char *cmd_output_str;
+    char *defn_function_name;
+    char *defn_function_ret_value_name;
+    char *defn_function_reigon_len;
 
-	char *cmd_file_open_identifier;
-	char *cmd_file_open_mode;
-	char *cmd_file_read_identifier;
-	char *cmd_file_read_variable;
-	char *cmd_file_write_identifier;
-	char *cmd_file_write_filename;
-	char *cmd_file_close_identifier;
-	char *cmd_file_seek_identifier;
-	double cmd_file_seek_address;
-	char *cmd_record_get_identifier;
-	char *cmd_record_get_variable;
-	char *cmd_record_put_identifier;
-	char *cmd_record_put_variable;
+    char *cmd_call_name;
+    char *cmd_input_name;
+    char *cmd_output_str;
+
+    char *cmd_file_open_identifier;
+    char *cmd_file_open_mode;
+    char *cmd_file_read_identifier;
+    char *cmd_file_read_variable;
+    char *cmd_file_write_identifier;
+    char *cmd_file_write_filename;
+    char *cmd_file_close_identifier;
+    char *cmd_file_seek_identifier;
+    char *cmd_file_seek_address;
+    char *cmd_record_get_identifier;
+    char *cmd_record_get_variable;
+    char *cmd_record_put_identifier;
+    char *cmd_record_put_variable;
 };
 
 
@@ -167,56 +167,56 @@ struct Instr {
 /**** Language grammar: ****
 
 <statement> ::=
-	| <var_decl>
-	| <const_decl>
-	| <array_decl>
-	| <input>
-	| <output>
-	| <openfile>
-	| <readfile>
-	| <writefile>
-	| <closefile>
-	| <seek>
-	| <getrecord>
-	| <putrecord>
-	| <custom_type_defn>
-	| <assignment>
-	| <conditional>
-	| <case>
-	| <for_loop>
-	| <repeat_until_loop>
-	| <while_loop>
-	| <proc_defn>
-	| <call>
-	| <fn_defn>
+    | <var_decl>
+    | <const_decl>
+    | <array_decl>
+    | <input>
+    | <output>
+    | <openfile>
+    | <readfile>
+    | <writefile>
+    | <closefile>
+    | <seek>
+    | <getrecord>
+    | <putrecord>
+    | <custom_type_defn>
+    | <assignment>
+    | <conditional>
+    | <case>
+    | <for_loop>
+    | <repeat_until_loop>
+    | <while_loop>
+    | <proc_defn>
+    | <call>
+    | <fn_defn>
 
 <comment> ::= "//" <text> <newline>
 
 <atomic_typename> ::= "INTEGER" | "REAL" | "CHAR" | "STRING" | "BOOLEAN" | "DATE"
 
 <literal> ::=
-	| <integer_literal>
-	| <real_literal>
-	| <char_literal>
-	| <string_literal>
-	| <boolean_literal>
-	| <date_literal>
+    | <integer_literal>
+    | <real_literal>
+    | <char_literal>
+    | <string_literal>
+    | <boolean_literal>
+    | <date_literal>
 
 <integer_literal> ::=
-	| <number>
-	| "-" <number>
+    | <number>
+    | "-" <number>
 
 <real_literal> ::=
-	| <number> "." <number>
-	| "-" <number> "." <number>
+    | <number> "." <number>
+    | "-" <number> "." <number>
 
 <char_literal> ::=
-	| "'" <character> "'"
-	| "'" "'" // Error case
+    | "'" <character> "'"
+    | "'" "'" // Error case
 
 <string_literal> ::=
-	| """ <string_text> """
-	| """ """
+    | """ <string_text> """
+    | """ """
 
 <boolean_literal> ::= "TRUE" | "FALSE"
 
@@ -234,8 +234,8 @@ struct Instr {
 <assignment> ::= <expression> "<-" <expression>
 
 <array_decl> ::=
-	| "DECLARE" <identifier> ":" "ARRAY" "[" <l> ":" <u> "]" "OF" <datatype>
-	| "DECLARE" <identifier> ":" "ARRAY" "[" <l1> ":" <u1> "," <l2> ":" <u2> "]" "OF" <datatype>
+    | "DECLARE" <identifier> ":" "ARRAY" "[" <l> ":" <u> "]" "OF" <datatype>
+    | "DECLARE" <identifier> ":" "ARRAY" "[" <l1> ":" <u1> "," <l2> ":" <u2> "]" "OF" <datatype>
 
 <custom_type_defn> ::= "TYPE" <identifier> <decl> "ENDTYPE"
 
@@ -245,40 +245,40 @@ struct Instr {
 <comma_seperated_values> ::= <value> "," <comma_seperated_values>
 
 <conditional> ::=
-	| "IF" <expr> "THEN" <statement> "ENDIF"
-	| "IF" <expr> "THEN" <statement> "ELSE" <statement> "ENDIF"
+    | "IF" <expr> "THEN" <statement> "ENDIF"
+    | "IF" <expr> "THEN" <statement> "ELSE" <statement> "ENDIF"
 
 <case> ::=
-	| "CASE" "OF" <identifier> <clause> "ENDCASE"
-	| "CASE" "OF" <identifier> <clause> "OTHERWISE" <statement> "ENDCASE"
+    | "CASE" "OF" <identifier> <clause> "ENDCASE"
+    | "CASE" "OF" <identifier> <clause> "OTHERWISE" <statement> "ENDCASE"
 <clause> ::=
-	| <value> ":" <statement>
-	| <clause>
+    | <value> ":" <statement>
+    | <clause>
 
 <for_loop> ::=
-	| "FOR" <identifier> "<-" <identifier> "TO" <identifier> <statement> "ENDFOR"
-	| "FOR" <identifier> "<-" <identifier> "TO" <identifier> "STEP" <identifier> <statement> "ENDFOR"
+    | "FOR" <identifier> "<-" <identifier> "TO" <identifier> <statement> "ENDFOR"
+    | "FOR" <identifier> "<-" <identifier> "TO" <identifier> "STEP" <identifier> <statement> "ENDFOR"
 
 <repeat_until_loop> ::= "REPEAT" <statement> "UNTIL" <expr>
 
 <while_loop> ::= "WHILE" <expr> "DO" <statement> "ENDWHILE"
 
 <proc_defn> ::=
-	| "PROCEDURE" <identifier> <statement> "ENDPROCEDURE"
-	| "PROCEDURE" <identifier> "(" <params> ")" <statement> "ENDPROCEDURE"
+    | "PROCEDURE" <identifier> <statement> "ENDPROCEDURE"
+    | "PROCEDURE" <identifier> "(" <params> ")" <statement> "ENDPROCEDURE"
 <params> ::=
-	| <params> "," <params>
-	| <identifier> ":" <identifier>
-	| "BYREF" <identifier> ":" <identifier>
-	| "BYVALUE" <identifier> ":" <identifier>
+    | <params> "," <params>
+    | <identifier> ":" <identifier>
+    | "BYREF" <identifier> ":" <identifier>
+    | "BYVALUE" <identifier> ":" <identifier>
 
 <call> ::=
-	| "CALL" <identifier>
-	| "CALL" <identifier> "(" <params> ")"
+    | "CALL" <identifier>
+    | "CALL" <identifier> "(" <params> ")"
 
 <fn_defn> ::=
-	| "FUNCTION" <identifier> "RETURNS" <identifier> <statement> "ENDFUNCTION"
-	| "FUNCTION" <identifier> "(" <params> ")" "RETURNS" <identifier> <statement> "ENDFUNCTION"
+    | "FUNCTION" <identifier> "RETURNS" <identifier> <statement> "ENDFUNCTION"
+    | "FUNCTION" <identifier> "(" <params> ")" "RETURNS" <identifier> <statement> "ENDFUNCTION"
 
 <openfile> ::= "OPENFILE" <identifier> "FOR" <filemode>
 <filemode> ::= "READ" | "WRITE" | "APPEND"
