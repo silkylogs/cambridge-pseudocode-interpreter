3026bd53f5a0885c07938a0dcbaac63057b30db3
silkylogs <silkylogs@gmail.com>
2026-01-07 15:46:12 +0600

    build.zsh: Update file names

--- cpi_c/build.zsh
+++ cpi_c/build.zsh
@@ -1,8 +1,8 @@
 clear
 FLAGS="-Wall -Wextra -Wmost -g -Wno-unsafe-buffer-usage"
 
-clang -c $FLAGS bless.c -o bless.o
-clang bless.o $FLAGS "test.c" -o "test"
+clang -c $FLAGS vm.c -o vm.o
+clang vm.o $FLAGS test.c -o "test"
 # clang main.c $FLAGS -o cpi_c
 
 ./"test"
