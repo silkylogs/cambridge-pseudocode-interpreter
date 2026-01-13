3322a72b49ce25dc98e5b3e0be0ac8d1435ef580
silkylogs <silkylogs@gmail.com>
2026-01-13 16:45:12 +0600

    vm.h: Fix formatting on comment... I think

--- cpi_c/vm.h
+++ cpi_c/vm.h
@@ -321,141 +321,138 @@ void vm_exe_instr(struct VmState *state, struct Instr *instr);
 
 #endif
 
-/**** Language grammar: ****
+// clang-format off
+// Language grammar:
+//
+//<statement> ::=
+//    | <var_decl>
+//    | <const_decl>
+//    | <array_decl>
+//    | <input>
+//    | <output>
+//    | <openfile>
+//    | <readfile>
+//    | <writefile>
+//    | <closefile>
+//    | <seek>
+//    | <getrecord>
+//    | <putrecord>
+//    | <custom_type_defn>
+//    | <assignment>
+//    | <conditional>
+//    | <case>
+//    | <for_loop>
+//    | <repeat_until_loop>
+//    | <while_loop>
+//    | <proc_defn>
+//    | <call>
+//    | <fn_defn>
+//
+//<comment> ::= "//" <text> <newline>
+//
+//<atomic_typename> ::= "INTEGER" | "REAL" | "CHAR" | "STRING" | "BOOLEAN" | "DATE"
+//
+//<literal> ::=
+//    | <integer_literal>
+//    | <real_literal>
+//    | <char_literal>
+//    | <string_literal>
+//    | <boolean_literal>
+//    | <date_literal>
+//
+//<integer_literal> ::=
+//    | <number>
+//    | "-" <number>
+//
+//<real_literal> ::=
+//    | <number> "." <number>
+//    | "-" <number> "." <number>
+//
+//<char_literal> ::=
+//    | "'" <character> "'"
+//    | "'" "'" // Error case
+//
+//<string_literal> ::=
+//    | """ <string_text> """
+//    | """ """
+//
+//<boolean_literal> ::= "TRUE" | "FALSE"
+//
+//<date_literal> ::= <day> "/" <month> "/" <year>
+//<day> ::= <digit><digit>
+//<month> ::= <digit><digit>
+//<year> ::= <digit><digit><digit><digit>
+//
+//<identifier> ::= <letter>, ( <letter> | <digit> )
+//
+//<var_decl> ::= DECLARE <identifier> : <datatype>
+//
+//<const_decl> ::= CONSTANT <identifier> = <literal>
+//
+//<assignment> ::= <expression> "<-" <expression>
+//
+//<array_decl> ::=
+//    | "DECLARE" <identifier> ":" "ARRAY" "[" <l> ":" <u> "]" "OF" <datatype>
+//    | "DECLARE" <identifier> ":" "ARRAY" "[" <l1> ":" <u1> "," <l2> ":" <u2> "]" "OF" <datatype>
+//
+//<custom_type_defn> ::= "TYPE" <identifier> <decl> "ENDTYPE"
+//
+//<input> ::= "INPUT" <identifier>
+//
+//<output> ::= "OUTPUT" <comma_seperated_values>
+//<comma_seperated_values> ::= <value> "," <comma_seperated_values>
+//
+//<conditional> ::=
+//    | "IF" <expr> "THEN" <statement> "ENDIF"
+//    | "IF" <expr> "THEN" <statement> "ELSE" <statement> "ENDIF"
+//
+//<case> ::=
+//    | "CASE" "OF" <identifier> <clause> "ENDCASE"
+//    | "CASE" "OF" <identifier> <clause> "OTHERWISE" <statement> "ENDCASE"
+//<clause> ::=
+//    | <value> ":" <statement>
+//    | <clause>
+//
+//<for_loop> ::=
+//    | "FOR" <identifier> "<-" <identifier> "TO" <identifier> <statement> "ENDFOR" 
+//    | "FOR" <identifier> "<-" <identifier> "TO" <identifier> "STEP" <identifier> <statement> "ENDFOR"
+//
+//<repeat_until_loop> ::= "REPEAT" <statement> "UNTIL" <expr>
+//
+//<while_loop> ::= "WHILE" <expr> "DO" <statement> "ENDWHILE"
+//
+//<proc_defn> ::=
+//    | "PROCEDURE" <identifier> <statement> "ENDPROCEDURE"
+//    | "PROCEDURE" <identifier> "(" <params> ")" <statement> "ENDPROCEDURE"
+//<params> ::=
+//    | <params> "," <params>
+//    | <identifier> ":" <identifier>
+//    | "BYREF" <identifier> ":" <identifier>
+//    | "BYVALUE" <identifier> ":" <identifier>
+//
+//<call> ::=
+//    | "CALL" <identifier>
+//    | "CALL" <identifier> "(" <params> ")"
+//
+//<fn_defn> ::=
+//    | "FUNCTION" <identifier> "RETURNS" <identifier> <statement> "ENDFUNCTION"
+//    | "FUNCTION" <identifier> "(" <params> ")" "RETURNS" <identifier> <statement> "ENDFUNCTION"
+//
+//<openfile> ::= "OPENFILE" <identifier> "FOR" <filemode>
+//<filemode> ::= "READ" | "WRITE" | "APPEND"
+//
+//<readfile> ::= "READFILE" <identifier> "," <variable>
+//
+//<writefile> ::= "WRITEFILE" <identifier> "," <variable>
+//
+//<closefile> ::= "CLOSEFILE" <identifier>
+//
+//<seek> ::= "SEEK" <identifier> "," <expr>
+//
+//<getrecord> ::= "GETRECORD" <identifier> "," <expr>
+//
+//<putrecord> ::= "PUTRECORD" <identifier> "," <expr>
+//
+//
+//<datatype> ::= <identifier> | <atomic_typename>
 
-<statement> ::=
-    | <var_decl>
-    | <const_decl>
-    | <array_decl>
-    | <input>
-    | <output>
-    | <openfile>
-    | <readfile>
-    | <writefile>
-    | <closefile>
-    | <seek>
-    | <getrecord>
-    | <putrecord>
-    | <custom_type_defn>
-    | <assignment>
-    | <conditional>
-    | <case>
-    | <for_loop>
-    | <repeat_until_loop>
-    | <while_loop>
-    | <proc_defn>
-    | <call>
-    | <fn_defn>
-
-<comment> ::= "//" <text> <newline>
-
-<atomic_typename> ::= "INTEGER" | "REAL" | "CHAR" | "STRING" | "BOOLEAN" |
-"DATE"
-
-<literal> ::=
-    | <integer_literal>
-    | <real_literal>
-    | <char_literal>
-    | <string_literal>
-    | <boolean_literal>
-    | <date_literal>
-
-<integer_literal> ::=
-    | <number>
-    | "-" <number>
-
-<real_literal> ::=
-    | <number> "." <number>
-    | "-" <number> "." <number>
-
-<char_literal> ::=
-    | "'" <character> "'"
-    | "'" "'" // Error case
-
-<string_literal> ::=
-    | """ <string_text> """
-    | """ """
-
-<boolean_literal> ::= "TRUE" | "FALSE"
-
-<date_literal> ::= <day> "/" <month> "/" <year>
-<day> ::= <digit><digit>
-<month> ::= <digit><digit>
-<year> ::= <digit><digit><digit><digit>
-
-<identifier> ::= <letter>, ( <letter> | <digit> )
-
-<var_decl> ::= DECLARE <identifier> : <datatype>
-
-<const_decl> ::= CONSTANT <identifier> = <literal>
-
-<assignment> ::= <expression> "<-" <expression>
-
-<array_decl> ::=
-    | "DECLARE" <identifier> ":" "ARRAY" "[" <l> ":" <u> "]" "OF" <datatype>
-    | "DECLARE" <identifier> ":" "ARRAY" "[" <l1> ":" <u1> "," <l2> ":" <u2> "]"
-"OF" <datatype>
-
-<custom_type_defn> ::= "TYPE" <identifier> <decl> "ENDTYPE"
-
-<input> ::= "INPUT" <identifier>
-
-<output> ::= "OUTPUT" <comma_seperated_values>
-<comma_seperated_values> ::= <value> "," <comma_seperated_values>
-
-<conditional> ::=
-    | "IF" <expr> "THEN" <statement> "ENDIF"
-    | "IF" <expr> "THEN" <statement> "ELSE" <statement> "ENDIF"
-
-<case> ::=
-    | "CASE" "OF" <identifier> <clause> "ENDCASE"
-    | "CASE" "OF" <identifier> <clause> "OTHERWISE" <statement> "ENDCASE"
-<clause> ::=
-    | <value> ":" <statement>
-    | <clause>
-
-<for_loop> ::=
-    | "FOR" <identifier> "<-" <identifier> "TO" <identifier> <statement>
-"ENDFOR" | "FOR" <identifier> "<-" <identifier> "TO" <identifier> "STEP"
-<identifier> <statement> "ENDFOR"
-
-<repeat_until_loop> ::= "REPEAT" <statement> "UNTIL" <expr>
-
-<while_loop> ::= "WHILE" <expr> "DO" <statement> "ENDWHILE"
-
-<proc_defn> ::=
-    | "PROCEDURE" <identifier> <statement> "ENDPROCEDURE"
-    | "PROCEDURE" <identifier> "(" <params> ")" <statement> "ENDPROCEDURE"
-<params> ::=
-    | <params> "," <params>
-    | <identifier> ":" <identifier>
-    | "BYREF" <identifier> ":" <identifier>
-    | "BYVALUE" <identifier> ":" <identifier>
-
-<call> ::=
-    | "CALL" <identifier>
-    | "CALL" <identifier> "(" <params> ")"
-
-<fn_defn> ::=
-    | "FUNCTION" <identifier> "RETURNS" <identifier> <statement> "ENDFUNCTION"
-    | "FUNCTION" <identifier> "(" <params> ")" "RETURNS" <identifier>
-<statement> "ENDFUNCTION"
-
-<openfile> ::= "OPENFILE" <identifier> "FOR" <filemode>
-<filemode> ::= "READ" | "WRITE" | "APPEND"
-
-<readfile> ::= "READFILE" <identifier> "," <variable>
-
-<writefile> ::= "WRITEFILE" <identifier> "," <variable>
-
-<closefile> ::= "CLOSEFILE" <identifier>
-
-<seek> ::= "SEEK" <identifier> "," <expr>
-
-<getrecord> ::= "GETRECORD" <identifier> "," <expr>
-
-<putrecord> ::= "PUTRECORD" <identifier> "," <expr>
-
-
-<datatype> ::= <identifier> | <atomic_typename>
-*/
