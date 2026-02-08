907b4a1a8d98c5748055e02cee1812cb6d04e303
silkylogs <silkylogs@gmail.com>
2026-02-08 22:00:26 +0600

    cpi_cpp: Relocate forth; Fix CMakeLists; Add simple example

--- cpi_cpp/src/forth.cpp
+++ cpi_cpp/src/forth.cpp
@@ -88,3 +88,115 @@ void Forth::Util::add_new_entry(State& self, char* const entry_as_byte_string, s
     *self.dict_latest_defined_entry = old_dict_latest_defined_entry;
     throw std::exception {"todo"};
 }
+
+/*
+#include <iostream>
+#include "forth.hpp"
+
+void exit(Forth::State& self) {
+    printf("Exiting...\n");
+    self.exit_requested = true;
+};
+void nop(Forth::State& self) {
+    (void)self;
+    printf("nop ");
+};
+void dpush(Forth::State& self) {
+    (*self.ptr_data_stack)++;
+}
+void dpop(Forth::State& self) {
+    (*self.ptr_data_stack)--;
+}
+void rpush(Forth::State& self) {
+    (*self.ptr_return_stack)++;
+}
+void rpop(Forth::State& self) {
+    (*self.ptr_return_stack)--;
+}
+
+// Pushes the cell next to that instruction to the stack, then adjusts the return accordingly
+void lit(Forth::State& self) {
+    auto literal_value = self.memory[self.memory[*self.ptr_return_stack]];
+
+    // Push literal value
+    (*self.ptr_data_stack)++;
+    self.memory[*self.ptr_data_stack] = literal_value;
+
+    // Dont return to the literal, return to the addr one beyond it.
+    self.memory[*self.ptr_return_stack]++;
+}
+void printchar(Forth::State& self) {
+    printf("%c", self.memory[*self.ptr_data_stack]); (*self.ptr_data_stack)--;
+}
+void brz(Forth::State& self) {
+    Cell addr = self.memory[*self.ptr_data_stack];
+    (*self.ptr_data_stack)--;
+    Cell cond = self.memory[*self.ptr_data_stack];
+    (*self.ptr_data_stack)--;
+    if (!cond) {
+        self.memory[*self.ptr_return_stack] = addr;
+    }
+}
+void sto(Forth::State& self) {
+    Cell addr = self.memory[*self.ptr_data_stack];
+    (*self.ptr_data_stack)--;
+    Cell val = self.memory[*self.ptr_data_stack];
+    (*self.ptr_data_stack)--;
+    self.memory[addr] = val;
+}
+void lod_cell(Forth::State& self) {
+    Cell addr = self.memory[*self.ptr_data_stack];
+    self.memory[*self.ptr_data_stack] = self.memory[addr];
+}
+void inc(Forth::State& self) { self.memory[*self.ptr_data_stack]++; }
+
+std::vector<void(*)(Forth::State&)> prims{
+    exit, nop, dpush, dpop,
+    rpush, rpop, lit, printchar,
+    brz, sto, lod_cell, inc,
+};
+
+int main() {
+    // TODO: High level usage
+    //ForthState fs {};
+    //fs.addprim("PrintHello", [](ForthState &self) { std::cout << "Hello, world\n"; });
+    //volatile auto *mem = fs.get_ptr_to_memory(); // For viewing through a debugger
+    //fs.execute("PrintHello");
+    
+
+    Forth::State fs{};
+    char rom[] =
+        "\x3D\x00""\x0F\x00""\x17\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00" // 0
+        "\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00"
+        "\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC" // 10
+        "\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC"
+        "\x00\x00""\x02\x80""exit"              "\x20\x00""\x00\x00""\x00\x00""\x00\x00" // 20
+        "\x20\x00""\x02\x80""pchr"              "\x28\x00""\x07\x00""\x00\x00""\x00\x00"
+        "\x28\x00""\x02\x80""ltrl"              "\x30\x00""\x06\x00""\x00\x00""\x00\x00" // 30
+        "\x30\x00""\x02\x00""main"              "\x38\x00"
+        "\x34\x00""\x48\x00""\x2C\x00"
+        "\x34\x00""\x65\x00""\x2C\x00"
+        "\x34\x00""\x6C\x00""\x2C\x00"
+        "\x34\x00""\x6C\x00""\x2C\x00"
+        "\x34\x00""\x6F\x00""\x2C\x00"
+        "\x34\x00""\x2C\x00""\x2C\x00"
+        "\x34\x00""\x20\x00""\x2C\x00"
+        "\x34\x00""\x77\x00""\x2C\x00"
+        "\x34\x00""\x6F\x00""\x2C\x00"
+        "\x34\x00""\x72\x00""\x2C\x00"
+        "\x34\x00""\x6C\x00""\x2C\x00"
+        "\x34\x00""\x64\x00""\x2C\x00"
+        "\x34\x00""\x21\x00""\x2C\x00"
+        "\x34\x00""\x0A\x00""\x2C\x00"
+        "\x24\x00"
+        ;
+    Forth::Util::alloc_set_memory_conents(fs, rom, sizeof(rom));
+    Forth::Util::fixup_ptrs_after_allocation(fs);
+    fs.primitives = prims;
+    do {
+        Forth::inner_interpreter(fs);
+    } while (!fs.exit_requested);
+
+    return 0;
+}
+*/
--- cpi_cpp/src/main.cpp
+++ cpi_cpp/src/main.cpp
@@ -1,109 +1,89 @@
-#include <iostream>
-#include "forth.hpp"
+﻿#include <print>
+#include <map>
+#include <vector>
+#include <string>
+#include <cassert>
 
-void exit(Forth::State& self) {
-    printf("Exiting...\n");
-    self.exit_requested = true;
+using Data = std::vector<unsigned char>;
+struct Variable {
+    std::string type;
+    Data data;
 };
-void nop(Forth::State& self) {
-    (void)self;
-    printf("nop ");
+using ScopeVars = std::map<std::string, Variable>;
+//using Scopes = std::vector<ScopeVars>;
+//Scopes scopes;
+
+auto ltrim = [](std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
 };
-void dpush(Forth::State& self) {
-    (*self.ptr_data_stack)++;
-}
-void dpop(Forth::State& self) {
-    (*self.ptr_data_stack)--;
-}
-void rpush(Forth::State& self) {
-    (*self.ptr_return_stack)++;
-}
-void rpop(Forth::State& self) {
-    (*self.ptr_return_stack)--;
-}
 
-// Pushes the cell next to that instruction to the stack, then adjusts the return accordingly
-void lit(Forth::State& self) {
-    auto literal_value = self.memory[self.memory[*self.ptr_return_stack]];
+auto rtrim = [](std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+};
 
-    // Push literal value
-    (*self.ptr_data_stack)++;
-    self.memory[*self.ptr_data_stack] = literal_value;
+auto trim = [](std::string &s) {
+    ltrim(s);
+    rtrim(s);
+};
 
-    // Dont return to the literal, return to the addr one beyond it.
-    self.memory[*self.ptr_return_stack]++;
-}
-void printchar(Forth::State& self) {
-    printf("%c", self.memory[*self.ptr_data_stack]); (*self.ptr_data_stack)--;
-}
-void brz(Forth::State& self) {
-    Cell addr = self.memory[*self.ptr_data_stack];
-    (*self.ptr_data_stack)--;
-    Cell cond = self.memory[*self.ptr_data_stack];
-    (*self.ptr_data_stack)--;
-    if (!cond) {
-        self.memory[*self.ptr_return_stack] = addr;
+// Parse program
+// - determine stmt type
+// - execute stmt
+static void exec_stmt(ScopeVars &vars, std::string stmt) {
+    trim(stmt);
+
+    auto first_space = stmt.find(" ");
+    std::string first_word = stmt.substr(0, first_space);
+
+    if ("DECLARE" == first_word) {
+        auto colon = stmt.find(":");
+        auto count = colon - first_space;
+        std::string var_name = stmt.substr(first_space, count);
+        trim(var_name);
+
+        std::string var_type = stmt.substr(colon + 1, stmt.size());
+        trim(var_type);
+
+        auto default_val_from_type = [](std::string var_type) -> Data {
+            assert("INTEGER" == var_type);
+            {
+                int64_t num = 0;
+                Data dat;
+                dat.resize(sizeof(int64_t));
+                memcpy(dat.data(), &num, sizeof(int64_t));
+                return dat;
+            }
+        };
+        Data default_value = default_val_from_type(var_type);
+
+        Variable v{ var_type, default_value };
+        vars[var_name] = v;
+    } else if ("OUTPUT" == first_word) {
+        auto output_var = stmt.substr(first_space + 1, stmt.size());
+        trim(output_var);
+
+        // TODO: Parse output
+
+        if (auto search = vars.find(output_var); search != vars.end()) {
+            auto v = search->second;
+            assert("INTEGER" == v.type);
+            {
+                int64_t val = *reinterpret_cast<int64_t *>(v.data.data());
+                std::print("{}", val);
+            }
+        } else {
+            std::print("Variable \"{}\" not found in this scope\n", output_var);
+            assert(0);
+        }
     }
 }
-void sto(Forth::State& self) {
-    Cell addr = self.memory[*self.ptr_data_stack];
-    (*self.ptr_data_stack)--;
-    Cell val = self.memory[*self.ptr_data_stack];
-    (*self.ptr_data_stack)--;
-    self.memory[addr] = val;
-}
-void lod_cell(Forth::State& self) {
-    Cell addr = self.memory[*self.ptr_data_stack];
-    self.memory[*self.ptr_data_stack] = self.memory[addr];
-}
-void inc(Forth::State& self) { self.memory[*self.ptr_data_stack]++; }
-
-std::vector<void(*)(Forth::State&)> prims{
-    exit, nop, dpush, dpop,
-    rpush, rpop, lit, printchar,
-    brz, sto, lod_cell, inc,
-};
 
 int main() {
-    /* TODO: High level usage
-    ForthState fs {};
-    fs.addprim("PrintHello", [](ForthState &self) { std::cout << "Hello, world\n"; });
-    volatile auto *mem = fs.get_ptr_to_memory(); // For viewing through a debugger
-    fs.execute("PrintHello");
-    */
-
-    Forth::State fs{};
-    char rom[] =
-        "\x3D\x00""\x0F\x00""\x17\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00" // 0
-        "\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00""\x00\x00"
-        "\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC" // 10
-        "\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC""\xCC\xCC"
-        "\x00\x00""\x02\x80""exit"              "\x20\x00""\x00\x00""\x00\x00""\x00\x00" // 20
-        "\x20\x00""\x02\x80""pchr"              "\x28\x00""\x07\x00""\x00\x00""\x00\x00"
-        "\x28\x00""\x02\x80""ltrl"              "\x30\x00""\x06\x00""\x00\x00""\x00\x00" // 30
-        "\x30\x00""\x02\x00""main"              "\x38\x00"
-        "\x34\x00""\x48\x00""\x2C\x00"
-        "\x34\x00""\x65\x00""\x2C\x00"
-        "\x34\x00""\x6C\x00""\x2C\x00"
-        "\x34\x00""\x6C\x00""\x2C\x00"
-        "\x34\x00""\x6F\x00""\x2C\x00"
-        "\x34\x00""\x2C\x00""\x2C\x00"
-        "\x34\x00""\x20\x00""\x2C\x00"
-        "\x34\x00""\x77\x00""\x2C\x00"
-        "\x34\x00""\x6F\x00""\x2C\x00"
-        "\x34\x00""\x72\x00""\x2C\x00"
-        "\x34\x00""\x6C\x00""\x2C\x00"
-        "\x34\x00""\x64\x00""\x2C\x00"
-        "\x34\x00""\x21\x00""\x2C\x00"
-        "\x34\x00""\x0A\x00""\x2C\x00"
-        "\x24\x00"
-        ;
-    Forth::Util::alloc_set_memory_conents(fs, rom, sizeof(rom));
-    Forth::Util::fixup_ptrs_after_allocation(fs);
-    fs.primitives = prims;
-    do {
-        Forth::inner_interpreter(fs);
-    } while (!fs.exit_requested);
-
-    return 0;
+    ScopeVars dat;
+    exec_stmt(dat, "DECLARE Foo : INTEGER");
+    exec_stmt(dat, "OUTPUT Foo");
 }
