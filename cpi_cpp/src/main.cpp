3798615106263cf1c9f3f333dd8e0bd4fddffdcb
silkylogs <silkylogs@gmail.com>
2026-02-09 18:29:06 +0600

    cpi_cpp: Add testing harness; Add explanatory comments

--- cpi_cpp/src/main.cpp
+++ cpi_cpp/src/main.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>
 #include <sstream>
 #include <variant>
+#include <functional>
+#include <ranges>
 
 #include "util.hpp"
 
@@ -23,13 +25,17 @@ const std::string adt_integer = "integer";
 static void exec_stmt(VarsInScope &vars, std::string stmt) {
     trim(stmt);
 
+    // First word hints at the statement type
     std::string first_word;
     auto first_space = stmt.find(" ");
     first_word = stmt.substr(0, first_space);
     lower(first_word);
 
     if ("declare" == first_word) {
+        // Name : type splitter
         auto colon = stmt.find(name_type_sep);
+
+        // Extract name
         std::string var_name = stmt.substr(first_space, colon - first_space);
         trim(var_name);
         lower(var_name);
@@ -38,10 +44,12 @@ static void exec_stmt(VarsInScope &vars, std::string stmt) {
             exit(0);
         }
 
+        // Extract type
         std::string var_type = stmt.substr(colon + name_type_sep.size(), stmt.size());
         trim(var_type);
         lower(var_type);
 
+        // Create default value
         auto default_val_from_type = [](std::string var_type) -> Data {
             assert(adt_integer == var_type);
             {
@@ -52,15 +60,16 @@ static void exec_stmt(VarsInScope &vars, std::string stmt) {
         };
         Data default_value = default_val_from_type(var_type);
 
+        // Assign default value
         VarData v{ var_type, default_value };
         vars.insert(std::make_pair(var_name, v));
     } else if ("output" == first_word) {
+        // Get output expr
         constexpr auto space_sep = std::string(" ").size();
         auto output_var = stmt.substr(first_space + space_sep, stmt.size());
         trim(output_var);
 
-        // TODO: Parse output
-
+        // Eval output expr
         if (auto search = vars.find(output_var); search != vars.end()) {
             auto &v = search->second;
             std::visit([](auto &&arg) { std::print("{}", arg); }, v.data);
@@ -75,14 +84,39 @@ static void exec_stmt(VarsInScope &vars, std::string stmt) {
 
 // TODO: Implement tests. Every branch of this code.
 
+struct Test {
+    std::string name;
+    std::function<bool()> fn;
+};
+
+Test tst(std::string n, std::function<bool()> fn) { return Test(n, fn); }
+
+bool run_tests() {
+    std::vector<Test> tests = {
+        tst("Failing", []() -> bool { 
+            return false;
+        }),
+    };
+
+    bool all_ok = true;
+    for (auto&& [index, test] : std::views::enumerate(tests)) {
+        bool ok = test.fn();
+        std::print("{} of {}: Test \"{}\"... {}\n", index+1, tests.size(), test.name, ok);
+        all_ok &= ok;
+    }
+    return all_ok;
+}
+
 int main() {
-    VarsInScope dat;
-    exec_stmt(dat, "declare foo: integer");
-    exec_stmt(dat, "declare bar: integer");
+    assert(run_tests());
 
-    exec_stmt(dat, "foo = 1");
-    exec_stmt(dat, "bar <- 2");
+    //VarsInScope dat;
+    //exec_stmt(dat, "declare foo: integer");
+    //exec_stmt(dat, "declare bar: integer");
 
-    exec_stmt(dat, "output foo");
-    exec_stmt(dat, "output bar");
+    //exec_stmt(dat, "foo = 1");
+    //exec_stmt(dat, "bar <- 2");
+
+    //exec_stmt(dat, "output foo");
+    //exec_stmt(dat, "output bar");
 }
