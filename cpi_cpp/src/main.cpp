0144d2a556470cb337f79dada787e6dd8c8c98d4
silkylogs <silkylogs@gmail.com>
2026-02-12 20:16:08 +0600

    cpi_cpp: Implement assignment

--- cpi_cpp/src/main.cpp
+++ cpi_cpp/src/main.cpp
@@ -21,32 +21,39 @@ using Scopes = std::vector<VarsInScope>;
 const std::string name_type_sep = ":";
 const std::string adt_integer = "integer";
 
+auto split = [](std::string s) -> std::tuple<std::string, std::string> {
+    auto space = s.find(" ");
+    return std::make_tuple(
+        s.substr(0, space),
+        s.substr(space + 1, s.size())
+    );
+};
+
+auto name_colon_type = [](std::string s) -> std::tuple<std::string, std::string> {
+    auto colon = s.find(":");
+    return std::make_tuple(
+        s.substr(0, colon),
+        s.substr(colon + 1, s.size())
+    );
+};
+
 static bool exec_stmt(VarsInScope &vars, std::string stmt) {
     trim(stmt);
 
     // First word hints at the statement type
-    std::string first_word;
-    auto first_space = stmt.find(" ");
-    first_word = stmt.substr(0, first_space);
+    auto [first_word, rest] = split(stmt);
     lower(first_word);
 
     if ("declare" == first_word) {
         // Name : type splitter
-        auto colon = stmt.find(name_type_sep);
+        auto [name, type] = name_colon_type(rest);
 
-        // Extract name
-        std::string var_name = stmt.substr(first_space, colon - first_space);
-        trim(var_name);
-        lower(var_name);
-        if (vars.contains(var_name)) {
-            std::print("Variable \"{}\" declared previously in this scope.\n", var_name);
+        trim(name);
+        lower(name);
+        if (vars.contains(name)) {
+            std::println("Variable \"{}\" declared previously in this scope.", name);
             return false;
         }
-
-        // Extract type
-        std::string var_type = stmt.substr(colon + name_type_sep.size(), stmt.size());
-        trim(var_type);
-        lower(var_type);
 
         // Create default value
         auto default_val_from_type = [](std::string var_type) -> Data {
@@ -57,30 +64,66 @@ static bool exec_stmt(VarsInScope &vars, std::string stmt) {
                 return dat;
             }
         };
-        Data default_value = default_val_from_type(var_type);
+        trim(type);
+        lower(type);
+        Data default_value = default_val_from_type(type);
 
         // Assign default value
-        VarData v{ var_type, default_value };
-        vars.insert(std::make_pair(var_name, v));
+        VarData v{ type, default_value };
+        vars.insert(std::make_pair(name, v));
     } else if ("output" == first_word) {
         // Get output expr
-        constexpr auto space_sep = std::string(" ").size();
-        auto output_var = stmt.substr(first_space + space_sep, stmt.size());
-        trim(output_var);
+        trim(rest);
 
         // Eval output expr
-        if (auto search = vars.find(output_var); search != vars.end()) {
+        if (auto search = vars.find(rest); search != vars.end()) {
             auto &v = search->second;
             std::visit([](auto &&arg) { std::print("{}", arg); }, v.data);
         } else {
-            std::print("Variable \"{}\" not found in this scope\n", output_var);
+            std::println("Variable \"{}\" not found in this scope", rest);
             return false;
         }
     } else { // Assignment
-        assert(!"TODO");
-        return false;
-    }
+        std::string ass_op_eq = "=";
+        std::string ass_op_ar = "<-";
+        std::string assop;
 
+        auto &lhs = first_word;
+
+        ltrim(rest);
+        auto loc_eq = rest.find(ass_op_eq);
+        auto loc_arrow = rest.find(ass_op_ar);
+        if (loc_eq == 0) {
+            assop = ass_op_eq;
+        } else if (loc_arrow == 0) {
+            assop = ass_op_ar;
+        } else {
+            std::println("Assignment operator not found at beginning of rhs");
+            return false;
+        }
+
+        auto rhs = rest.substr(rest.find(assop)+assop.size(), rest.size());
+        trim(rhs);
+        lower(rhs);
+
+        if (auto search_lhs = vars.find(lhs); search_lhs != vars.end()) {
+            if (auto search_rhs = vars.find(rhs); search_rhs != vars.end()) {
+                if (search_lhs->second.type == search_rhs->second.type) {
+                    search_lhs->second.data = search_rhs->second.data;
+                    return true;
+                } else {
+                    std::println("LHS type ({}) and RHS type ({}) do not match", search_lhs->second.type, search_rhs->second.type);
+                    return false;
+                }
+            } else {
+                std::println("RHS \"{}\" not found in this scope", rhs);
+                return false;
+            }
+        } else {
+            std::println("LHS \"{}\" not found in this scope", lhs);
+            return false;
+        }
+    }
     return true;
 }
 
@@ -95,6 +138,11 @@ Test tst(std::string n, std::function<bool()> fn) { return Test(n, fn); }
 
 bool run_tests() {
     std::vector<Test> tests = {
+        tst("Split", []() -> bool {
+            auto [x,y] = split("DECLARE y");
+            return x == "DECLARE" && y == "y";
+        }),
+
         tst("Decl punctuation stress test", []() -> bool {
             VarsInScope dat;
 
@@ -140,28 +188,68 @@ bool run_tests() {
 
             return ok;
         }),
+
+        tst("Name:Type", []() -> bool {
+            auto [foo, bar] = name_colon_type("Foo:Bar");
+            return foo=="Foo"&&bar=="Bar";
+        }),
+
+        tst("Assignment", []() -> bool {
+            VarsInScope dat;
+            bool ok = true;
+            ok &= exec_stmt(dat, "declare foo: integer");
+            ok &= exec_stmt(dat, "declare bar: integer");
+
+            if (auto search = dat.find("foo"); search != dat.end()) {
+                auto &data = search->second.data;
+                ok &= 0 == std::get<int64_t>(data);
+            } else {
+                ok = false;
+            }
+
+            if (auto search = dat.find("bar"); search != dat.end()) {
+                search->second.data = 3;
+            } else {
+                ok = false;
+            }
+
+            ok &= exec_stmt(dat, "foo = bar");
+            if (auto search = dat.find("foo"); search != dat.end()) {
+                auto &data = search->second.data;
+                ok &= 3 == std::get<int64_t>(data);
+            } else {
+                ok = false;
+            }
+
+            if (auto search = dat.find("foo"); search != dat.end()) {
+                search->second.data = 5;
+            } else {
+                ok = false;
+            }
+
+            ok &= exec_stmt(dat, "bar <- foo");
+            if (auto search = dat.find("bar"); search != dat.end()) {
+                auto &data = search->second.data;
+                ok &= 5 == std::get<int64_t>(data);
+            } else {
+                ok = false;
+            }
+
+            return ok;
+        }),
     };
 
     bool all_ok = true;
     for (size_t i = 0; i < tests.size(); ++i) {
         auto &test = tests[i];
         bool ok = test.fn();
-        std::print("{} of {}: Test \"{}\"... {}\n", i + 1, tests.size(), test.name, ok);
+        std::println("{} of {}: Test \"{}\"... {}", i + 1, tests.size(), test.name, ok);
         all_ok &= ok;
     }
+    std::println("\nTest summary: all_ok={}", all_ok);
     return all_ok;
 }
 
 int main() {
     assert(run_tests());
-
-    //VarsInScope dat;
-    //exec_stmt(dat, "declare foo: integer");
-    //exec_stmt(dat, "declare bar: integer");
-
-    //exec_stmt(dat, "foo = 1");
-    //exec_stmt(dat, "bar <- 2");
-
-    //exec_stmt(dat, "output foo");
-    //exec_stmt(dat, "output bar");
 }
